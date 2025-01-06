cea2a35f21826ac7c41271c3a7deabb86ad71fd3
MuhamadRizkiRamadaniEdiyanto <muhamadrizki131103@gmail.com>
2025-01-06 23:19:43 +0700

    kelompok-6

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Beranda;
+use App\Livewire\User;
+use App\Livewire\Laporan;
+use App\Livewire\Produk;
+use App\Livewire\Transaksi;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+Auth::routes();
+
+Route::get('/home', Beranda::class)->middleware(['auth'])->name('home');
+Route::get('/user', User::class)->middleware(['auth'])->name('user');
+Route::get('/laporan', Laporan::class)->middleware(['auth'])->name('laporan');
+Route::get('/produk', produk::class)->middleware(['auth'])->name('produk');
+Route::get('/transaksi', Transaksi::class)->middleware(['auth'])->name('transaksi');
+Route::get('/cetak', ['App\Http\Controllers\HomeController', 'cetak']);
